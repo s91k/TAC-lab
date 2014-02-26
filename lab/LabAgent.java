a4afa54fc3dff60ebea9a060ec327020e5341868
msindhusha <msindhusha@yahoo.com>
2014-02-26 12:07:04 +0100

    changed merge

--- lab/LabAgent.java
+++ lab/LabAgent.java
@@ -232,16 +232,17 @@ public class LabAgent extends AgentImpl {
 		} 
 		else if (auctionCategory == TACAgent.CAT_ENTERTAINMENT) 
 		{
-			Bid bid = new Bid(auction);
-			//for (int t = 0; t < 8; t++)
-
-			int alloc = agent.getAllocation(auction);
-			int own = agent.getOwn(auction);
-			//selling all the tickets we are allocated                  
-			int rem=alloc-own;
-			if(rem>0)
-				prices[auction]=quote.getAskPrice()+10f;
-			bid.addBidPoint(rem, prices[auction]);
+			  Bid bid = new Bid(auction)
+       
+      			 int alloc = agent.getAllocation(auction);
+		       int own = agent.getOwn(auction);
+       //selling all the tickets we are allocated                  
+       int rem=alloc-own;
+       if(rem>0)
+       prices[auction]=quote.getAskPrice()+10f;
+       if(rem<0)
+       prices[auction]=agent.getBid(auction)-10f; 
+            bid.addBidPoint(rem, prices[auction]);
 
 			if (DEBUG) 
 			{
